7cce190f9ec6bdc7d19a73b04c198461236260cb
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2023-04-14 17:08:08 -0700

    Clean out string

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -5,8 +5,13 @@ import { Head } from "$fresh/runtime.ts";
 import NodeRSA from "https://esm.sh/node-rsa@1.1.1";
 import "$dotenv/load.ts";
 
-const privateKeyPEMString = Deno.env.get("KEY") as string;
+let privateKeyPEMString = Deno.env.get("KEY") as string;
+// remove the " characters if they are there
+privateKeyPEMString = privateKeyPEMString.replace(/"/g, "");
+// turn the \n into actual newlines
+privateKeyPEMString = privateKeyPEMString.replace(/\\n/g, "\n");
 console.log(privateKeyPEMString);
+
 const privateKeyPEM = new NodeRSA(privateKeyPEMString, "pkcs1");
 const privateKeyPkcs8 = privateKeyPEM.exportKey("pkcs8-private-pem");
 
