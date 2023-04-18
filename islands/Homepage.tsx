f424d36f0e6dc12faea05fe5e6ec59d319d2fde1
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2023-04-18 13:43:32 -0700

    style changes

--- islands/Homepage.tsx
+++ islands/Homepage.tsx
@@ -7,12 +7,14 @@ export default function Homepage() {
         <Header />
       </div>
       <div class="flex flex-col items-center justify-center h-screen absolute top-0 right-0 w-screen -z-50">
-        <h1 class="text-6xl font-bold text-gray-500 text-center px-4">Github Ranked</h1>
-        <p class="text-lg font-light text-gray-500 text-center px-4">Type a Github username is the searchbox above</p>
-        <p className="font-light text-sm text-gray-500 lg:w-80 pt-2 px-4 text-justify">
-          Github Ranked is a tool to easily showcase the most impressive contributions of a Github user and rank their account based on these. 
-          Using this tool, you can easily tell who is active on Github and what their area of interest is. 
-        </p>
+        <div>
+          <h1 class="text-6xl font-bold text-gray-500 px-4">Github Ranked</h1>
+          <p class="text-lg font-light text-gray-500 px-4">Type a Github username is the searchbox above</p>
+          <p className="font-light text-sm text-gray-500 lg:w-96 pt-2 px-4">
+            Github Ranked is a tool to easily showcase the most impressive contributions of a Github user and rank their account based on these. 
+            Using this tool, you can easily tell who is active on Github and what their area of interest is. 
+          </p>
+        </div>
       </div>
     </>
   )
