a6038f6dded691e205f07f3280fec110453f2df0
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2023-04-27 13:21:13 -0700

    Make fonts better

--- routes/api/generate.tsx
+++ routes/api/generate.tsx
@@ -5,8 +5,7 @@ import IconStarFilled from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/star-
 import IconBook2 from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/book-2.tsx"
 import IconTrophyFilled from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/trophy-filled.tsx"
 import { createCanvas, loadImage } from "https://deno.land/x/canvas/mod.ts";
-
-
+import * as path from "https://deno.land/std@0.183.0/path/mod.ts";
 
 
 export const handler: Handlers = {
@@ -17,9 +16,22 @@ export const handler: Handlers = {
       return new Response("Missing name query parameter", { status: 400 });
     }
     const { repos, user } = await getContributions(name);
-    const image = await loadImage(user.avatarUrl);
     const canvas = createCanvas(800, 200 + Math.min(10, repos.length) * 40);
+
+    let family = 'routes/api/OpenSans-SemiBold.ttf'
+    // resolve family to full path
+    //const __dirname = new URL('.', import.meta.url).pathname;
+    // family = path.resolve(__dirname, '../../data/static/OpenSans_SemiCondensed-Regular.ttf')
+    
+    // resolve path
+    const font = await Deno.readFile(family)
+    const font_identifier = new Date().toString()
+    canvas.loadFont(font, {
+      family: font_identifier
+    })
+
     const ctx = canvas.getContext("2d");
+    ctx.font = `24px ${font_identifier}`
     // make background white
     ctx.fillStyle = "#fff";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -35,7 +47,6 @@ export const handler: Handlers = {
 
     ctx.fillStyle = "#888";
     // make font Courier New
-    ctx.font = "22px Arial";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     const points = repos.reduce((acc: any, curr: any) => {
@@ -58,34 +69,69 @@ export const handler: Handlers = {
     const tableRowColor = "#ccc";
     const tableHeaderBgColor = "#f2f2f2";
     const tableRowBgColor = "#fff";
+    const cornerRadius = 10; // Change this to adjust the corner radius
 
     // Draw the table header
     ctx.fillStyle = tableHeaderBgColor;
-    ctx.fillRect(panelPadding, panelPadding + panelHeight / 4, panelWidth - panelPadding * 2, tableHeaderFontSize + panelPadding);
+    roundRectTop(ctx, panelPadding, panelPadding + panelHeight / 4, panelWidth - panelPadding * 2, tableHeaderFontSize + panelPadding, cornerRadius).fill();
     ctx.fillStyle = tableHeaderColor;
-    ctx.font = `${tableHeaderFontSize}px Arial`;
+    ctx.font = `${tableHeaderFontSize}px ${font_identifier}`
     ctx.fillText("Rank", panelPadding * 2, panelPadding * 2 + panelHeight / 4 + tableHeaderFontSize - 10);
     ctx.fillText("Name", panelPadding * 5, panelPadding * 2 + panelHeight / 4 + tableHeaderFontSize - 10);
     ctx.fillText("Stars", panelWidth / 2, panelPadding * 2 + panelHeight / 4 + tableHeaderFontSize - 10);
     ctx.fillText("Contributions", panelWidth - panelPadding * 4 - 80, panelPadding * 2 + panelHeight / 4 - 10 + tableHeaderFontSize);
 
     // Draw the table rows
-    let currentRowY = panelPadding * 3 + tableHeaderFontSize + panelHeight / 4;
+    let currentRowY = panelPadding * 2 + tableHeaderFontSize + panelHeight / 4;
     const numRows = Math.min(10, repos.length);
     for (let i = 0; i < numRows; i++) {
       const repo = repos[i];
       const rowColor = i % 2 === 0 ? tableRowColor : tableRowBgColor;
       ctx.fillStyle = rowColor;
-      ctx.fillRect(panelPadding, currentRowY, panelWidth - panelPadding * 2, tableRowFontSize + panelPadding);
+      if (i === numRows - 1) {
+        roundRect(ctx, panelPadding, currentRowY, panelWidth - panelPadding * 2, tableRowFontSize + panelPadding, cornerRadius).fill();
+        ctx.fillRect(panelPadding, currentRowY, panelWidth - panelPadding * 2, panelPadding / 2);
+      } else {
+        ctx.fillRect(panelPadding, currentRowY, panelWidth - panelPadding * 2, tableRowFontSize + panelPadding);
+      }
       ctx.fillStyle = textColor;
-      ctx.font = `${tableRowFontSize}px Arial`;
-      ctx.fillText(`${i + 1}`, panelPadding * 2, currentRowY + tableRowFontSize + panelPadding/2 - 5);
-      ctx.fillText(repo.repository.name, panelPadding * 5, currentRowY + tableRowFontSize + panelPadding/2 - 5);
-      ctx.fillText(repo.repository.stargazerCount.toString(), panelWidth / 2, currentRowY + tableRowFontSize + panelPadding/2 - 5);
-      ctx.fillText(repo.contributions.totalCount.toString(), panelWidth - panelPadding * 4, currentRowY + tableRowFontSize + panelPadding/2 - 5);
+      ctx.font = `${tableRowFontSize}px ${font_identifier}`;
+      ctx.fillText(`${i + 1}`, panelPadding * 2, currentRowY + tableRowFontSize + panelPadding/2 - 2.5);
+      ctx.fillText(repo.repository.name, panelPadding * 5, currentRowY + tableRowFontSize + panelPadding/2 - 2.5);
+      ctx.fillText(repo.repository.stargazerCount.toString(), panelWidth / 2, currentRowY + tableRowFontSize + panelPadding/2 - 2.5);
+      ctx.fillText(repo.contributions.totalCount.toString(), panelWidth - panelPadding * 4, currentRowY + tableRowFontSize + panelPadding/2 - 2.5);
       currentRowY += tableRowFontSize + panelPadding;
     }
     
+    function roundRect(ctx: any, x: any, y: any, width: any, height: any, radius: any) {
+      ctx.beginPath();
+      ctx.moveTo(x + radius, y);
+      ctx.lineTo(x + width - radius, y);
+      ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+      ctx.lineTo(x + width, y + height - radius);
+      ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+      ctx.lineTo(x + radius, y + height);
+      ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+      ctx.lineTo(x, y + radius);
+      ctx.quadraticCurveTo(x, y, x + radius, y);
+      ctx.closePath();
+      return ctx;
+    };
+
+    function roundRectTop(ctx: any, x: any, y: any, width: any, height: any, radius: any) {
+      ctx.beginPath();
+      ctx.moveTo(x + radius, y);
+      ctx.lineTo(x + width - radius, y);
+      ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+      ctx.lineTo(x + width, y + height);
+      ctx.lineTo(x, y + height);
+      ctx.lineTo(x, y + radius);
+      ctx.quadraticCurveTo(x, y, x + radius, y);
+      ctx.closePath();
+      return ctx;
+    }
+    
+
 
     const returnImage = canvas.toBuffer();
     return new Response(returnImage, {
