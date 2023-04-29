1c752796c6c68f9a8e98a7bcf58d2260759440c0
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2023-04-29 10:45:43 -0700

    add loading to header

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -22,34 +22,43 @@ export default function Header() {
       }
     }
   }
-
-  return (
-    <div className="py-3 px-2 border-b w-screen flex flex-row items-center text-gray-500 flex-wrap"> 
-      <a 
-        className="text-gray-500 transition duration-200 ease-in-out hover:text-gray-400 pl-2.5 md:pr-0"
-        href="/"
-      >
-        <IconHome class="w-8 h-8" />
-      </a>
-      <input 
-        type="text" 
-        placeholder="username" 
-        className="border rounded p-2 ml-4" 
-        value={username}
-        onInput={(e) => setUsername((e.target as HTMLInputElement).value)}
-      />
-      <button 
-        className="md:w-10 w-full bg-green-400 text-white rounded p-2 mx-4 my-2 md:ml-4 md:mr-0 font-bold transition duration-200 ease-in-out hover:bg-green-500"
-        onClick={processEnter}
-      >
-        GO
-      </button>
-      <button 
-        className="md:w-40 w-full bg-green-400 text-white rounded p-2 mx-4 my-2 md:ml-4 md:mr-0 font-bold transition duration-200 ease-in-out hover:bg-green-500"
-        onClick={processGenerate}
-      >
-        GENERATE IMAGE
-      </button>
-    </div>
-  );
+  //if (window.location?.href) {
+    return (
+      <>
+      {
+        window.location?.href ? 
+          (
+            <div className="py-3 px-2 border-b w-screen flex flex-row items-center text-gray-500 flex-wrap"> 
+              <a 
+                className="text-gray-500 transition duration-200 ease-in-out hover:text-gray-400 pl-2.5 md:pr-0"
+                href="/"
+              >
+                <IconHome class="w-8 h-8" />
+              </a>
+              <input 
+                type="text" 
+                placeholder="username" 
+                className="border rounded p-2 ml-4" 
+                value={username}
+                onInput={(e) => setUsername((e.target as HTMLInputElement).value)}
+              />
+              <button 
+                className="w-full md:w-10 bg-green-400 text-white rounded p-2 mx-4 my-2 md:ml-4 md:mr-0 font-bold transition duration-200 ease-in-out hover:bg-green-500"
+                onClick={processEnter}
+              >
+                GO
+              </button>
+              <button 
+                className="w-full md:w-40 bg-green-400 text-white rounded p-2 mx-4 my-2 md:ml-4 md:mr-0 font-bold transition duration-200 ease-in-out hover:bg-green-500"
+                onClick={processGenerate}
+              >
+                GENERATE IMAGE
+              </button>
+          </div>
+          ) : (
+            <div className="border-b py-3 px-2 h-16 w-screen flex flex-row items-center text-gray-500 flex-wrap" />
+          )
+      }
+      </>
+    );
 }
