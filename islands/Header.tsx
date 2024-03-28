992097ec697c700ab15aa2b258d9deb5fc342dad
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2024-03-28 11:26:42 -0700

    Fix header

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -25,39 +25,27 @@ export default function Header() {
   //if (window.location?.href) {
   return (
     <>
-      {window.location?.href
-        ? (
-          <div className="py-3 px-2 border-b w-screen flex flex-row items-center text-gray-500 flex-wrap">
-            <a
-              className="text-gray-500 transition duration-200 ease-in-out hover:text-gray-400 pl-2.5 md:pr-0"
-              href="/"
-            >
-              <IconHome class="w-8 h-8" />
-            </a>
-            <input
-              type="text"
-              placeholder="username"
-              className="border rounded p-2 ml-4"
-              value={username}
-              onInput={(e) => setUsername((e.target as HTMLInputElement).value)}
-            />
-            <button
-              className="w-full md:w-10 bg-green-400 text-white rounded p-2 mx-4 my-2 md:ml-4 md:mr-0 font-bold transition duration-200 ease-in-out hover:bg-green-500"
-              onClick={processEnter}
-            >
-              GO
-            </button>
-            <button
-              className="w-full md:w-40 bg-green-400 text-white rounded p-2 mx-4 my-2 md:ml-4 md:mr-0 font-bold transition duration-200 ease-in-out hover:bg-green-500"
-              onClick={processGenerate}
-            >
-              GENERATE IMAGE
-            </button>
-          </div>
-        )
-        : (
-          <div className="border-b py-3 px-2 h-16 w-screen flex flex-row items-center text-gray-500 flex-wrap" />
-        )}
+        <div className="py-3 px-2 border-b w-screen flex flex-row items-center text-gray-500 flex-wrap">
+          <a
+            className="text-gray-500 transition duration-200 ease-in-out hover:text-gray-400 pl-2.5 md:pr-0"
+            href="/"
+          >
+            <IconHome class="w-8 h-8" />
+          </a>
+          <input
+            type="text"
+            placeholder="username"
+            className="border rounded p-2 ml-4"
+            value={username}
+            onInput={(e) => setUsername((e.target as HTMLInputElement).value)}
+          />
+          <button
+            className="w-full md:w-40 bg-green-400 text-white rounded p-2 mx-4 my-2 md:ml-4 md:mr-0 font-bold transition duration-200 ease-in-out hover:bg-green-500"
+            onClick={processGenerate}
+          >
+            GENERATE IMAGE
+          </button>
+        </div>
     </>
   );
 }
